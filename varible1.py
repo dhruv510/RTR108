f40b439083d2e3298e0767e44c71d15d252ded8a
user <dhruvsavani510@gmail.com>
2020-02-20 10:06:00 +0200

    20200220_09_56

---
+++ varible1.py
@@ -0,0 +1,18 @@
+print(4)
+type('Hello, World!')
+print('Hello, World!')
+print('17')
+print('3.2')
+print(1,000,000)
+x = 2
+print(x)
+message = 'And now for something completely different'
+n = 17
+pi = 3.1415926535897931
+print(n)
+print(pi)
+type(message)
+type(n)
+type(pi)
+print('minute')
+type('hour')
