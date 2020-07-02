f6db7535b350f9bc5f0eb7b3fe9d174681203dca
dhruv510 <60730843+dhruv510@users.noreply.github.com>
2020-07-02 09:46:56 +0530

    Add files via upload

---
+++ Variables, expressions, and statements.py
@@ -0,0 +1,166 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(4)
+4
+>>> type('Hello, World!')
+<class 'str'>
+>>> type('Hello, World!')
+<class 'str'>
+>>> type(3.2)
+<class 'float'>
+>>> type('17')
+<class 'str'>
+>>> type('3.2')
+<class 'str'>
+>>> print(1,000,000)
+1 0 0
+>>> message = 'And now for something completely different'
+>>> n = 17
+>>> pi = 3.1415926535897931
+>>> print(n)
+17
+>>> print(pi)
+3.141592653589793
+>>> type(message)
+<class 'str'>
+>>> type(n)
+<class 'int'>
+>>> type(pi)
+<class 'float'>
+>>> 76trombones = 'big parade'
+SyntaxError: invalid syntax
+>>>  more@ = 1000000
+ 
+SyntaxError: unexpected indent
+>>> class = 'Advanced Theoretical Zymurgy'
+SyntaxError: invalid syntax
+>>> print(1)
+x = 2
+print(x)
+SyntaxError: multiple statements found while compiling a single statement
+>>> print(1)
+1
+>>> x = 2
+>>> print(x)
+2
+>>> minute = 59
+>>> minute/60
+0.9833333333333333
+>>> minute = 59
+>>> minute/60
+0.9833333333333333
+>>> minute = 59
+>>> minute//60
+0
+>>> 1 + 1
+2
+>>> 5
+5
+>>> x = 5
+>>> x + 1
+6
+>>> quotient = 7 // 3
+>>> print(quotient)
+2
+>>> print(quotient)
+2
+>>> print(remainder)
+Traceback (most recent call last):
+  File "<pyshell#35>", line 1, in <module>
+    print(remainder)
+NameError: name 'remainder' is not defined
+>>> remainder = 7 % 3
+>>> print(remainder)
+1
+>>> first = 10
+>>> second = 15
+>>> print(first+second)
+SyntaxError: multiple statements found while compiling a single statement
+>>> first = 10
+>>> second = 15
+
+>>> second = 15
+>>> print(first+second)
+25
+>>> first = '100'
+
+>>> second = '150'
+>>> print(first + second)
+100150
+>>> first = 'Test '
+>>> second = 3
+>>> print(first * second)
+Test Test Test 
+>>> inp = input()
+some silly stuff
+>>> print(inp)
+some silly stuff
+>>> name = input('What is your name?\n')
+What is your name?
+dhruv
+>>> print(name)
+dhruv
+>>> prompt = 'What...is the airspeed velocity of an unladen swallow?\n'
+>>> speed = input(prompt)
+What...is the airspeed velocity of an unladen swallow?
+17
+>>> int(speed)
+17
+>>> int(speed) + 5
+22
+>>> speed = input(prompt)
+What...is the airspeed velocity of an unladen swallow?
+What do you mean, an African or a European swallow?
+>>> int(speed)
+Traceback (most recent call last):
+  File "<pyshell#58>", line 1, in <module>
+    int(speed)
+ValueError: invalid literal for int() with base 10: 'What do you mean, an African or a European swallow?'
+>>> # compute the percentage of the hour that has elapsed
+percentage = (minute * 100) / 60
+>>> percentage = (minute * 100) / 60     # percentage of an hour
+>>> v = 5     # assign 5 to v
+>>> v = 5     # velocity in meters/second.
+>>> a = 35.0
+>>> b = 12.50
+>>> 
+KeyboardInterrupt
+>>> 
+>>> c = a * b
+>>> print(c)
+437.5
+
+>>> hours = 35.0
+>>> rate = 12.50
+>>> pay = hours * rate
+>>> print(pay)
+437.5
+>>> x1q3z9ahd = 35.0
+>>> x1q3z9afd = 12.50
+>>> x1q3p9afd = x1q3z9ahd * x1q3z9afd
+>>> print(x1q3p9afd)
+437.5
+>>> 
+>>> for word in words:
+	print(word)
+for slice in pizza:
+	
+SyntaxError: invalid syntax
+>>> for slice in pizza:
+	print(slice)
+
+Traceback (most recent call last):
+  File "<pyshell#81>", line 1, in <module>
+    for slice in pizza:
+NameError: name 'pizza' is not defined
+>>> bad name = 5
+SyntaxError: invalid syntax
+>>> principal = 327.68
+>>> interest = principle * rate
+Traceback (most recent call last):
+  File "<pyshell#84>", line 1, in <module>
+    interest = principle * rate
+NameError: name 'principle' is not defined
+>>> 1.0 / 2.0 * pi
+1.5707963267948966
+>>> 
